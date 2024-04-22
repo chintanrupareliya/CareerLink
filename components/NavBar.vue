7631f1c30c4746d22ed4bf0629fce87ae318698e
ChitanZignuts <chintanr@zignuts.com>
2024-04-22 17:33:47 +0530

    setting up project

---
+++ components/NavBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+const drawer = ref(false);
+</script>
+
+<template>
+  <div>
+    <!-- Navigation bar -->
+    <VAppBar>
+      <div class="d-flex ma-2">
+        <VAvatar size="avatarSize" color="red">
+          <img src="/Frame.png" alt="alt" />
+        </VAvatar>
+        <VToolbarTitle class="ml-2">My Company</VToolbarTitle>
+      </div>
+    </VAppBar>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div>
+    <NavBar />
+  </div>
+</template>
